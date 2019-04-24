ff3ae0f693364d96c600bcdc3bc41c0989f01ccd
qofw <haliban256@gmail.com>
2019-04-24 11:25:42 +0600

    launcher app

---
+++ coding Implementation/BookSharing/app/src/main/java/com/example/booksharing/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.booksharing;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogIn(View view) {
+        Intent intent= new Intent(this,LogIn.class);
+        startActivity(intent);
+    }
+
+    public void goToSignUp(View view) {
+        Intent intent= new Intent(this,SignUp.class);
+        startActivity(intent);
+    }
+}
